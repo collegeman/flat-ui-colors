326c417506856c67b76c3795e12792690133ec7e
Aaron Collegeman <aaron@collegeman.net>
2014-11-01 20:31:31 -0400

    Fix some bugs

--- README.md
+++ README.md
@@ -8,6 +8,17 @@ So I made [this tool](https://collegeman.github.io/flat-ui-colors).
 
 Twenty colors * seven shades = 140 colors = everything you need for beauty-through-color.
 
+# Who is Designmodo?
+
+Designmodo ([on the web](http://designmodo.com) and [on GitHub](https://github.com/designmodo))
+is a New York based company that creates awesome commercial User Interface Packs,
+including the highly-rated [Flat UI](http://designmodo.com/flat/)&mdash;a Bootstrap theme that includes
+some additional (and useful) components.
+
+It was through Designmodo that I encountered this amazing palette:
+
+![Look at all the pretty colors](http://cl.ly/image/3H18260g0o2i/Screen%20Shot%202014-11-01%20at%208.29.15%20PM.png)
+
 # How to contribute
 
 Want a feature? Find something wrong with the tool? Please fork and do a pull
--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
+!function() {
+  var shades = [
+        'darkest',
+        'darker',
+        'dark',
+        'normal',
+        'light',
+        'lighter',
+        'lightest'
+      ],
+      hexDigits = [
+        "0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"
+      ],
+      currentShade = 3,
+      $lastClicked = null,
+      $notice = $('.notice'),
+      $input = $notice.find('input'),
+      $swatch = $notice.find('.swatch');
 
+  function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    return hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
+
+  function hex(x) {
+    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+  }
+
+  function updateNotice() {
+    var rgb = $lastClicked.css('backgroundColor'),
+        hex = rgb2hex(rgb);
+    $notice.show();
+    $input.val(hex).select();
+    $swatch.css('backgroundColor', rgb);
+  }
+
+  $(window).on('darken-palette', function() {
+    currentShade--;
+    if (currentShade < 0) {
+      currentShade = 0;
+    }
+    $('body').removeClass().addClass(shades[currentShade]);
+    updateNotice();
+  });
+
+  $(window).on('lighten-palette', function() {
+    currentShade++;
+    if (currentShade === shades.length) {
+      currentShade = shades.length - 1;
+    }
+    $('body').removeClass().addClass(shades[currentShade]);
+    updateNotice();
+  });
+
+  $(window).on('reset-palette', function() {
+    currentShade = 3;
+    $('body').removeClass().addClass(shades[currentShade]);
+    updateNotice();
+  });
+
+  $('.row > *').click(function(e) {
+    $lastClicked = $(e.target);
+    updateNotice();
+    return false;
+  });
+
+}();
